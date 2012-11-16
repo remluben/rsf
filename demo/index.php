97405f32068f24dc932911a32f9a62b3c2d2087e
remluben <ulmer.benjamin@gmail.com>
2012-11-16 21:05:15 +0100

    Moved part of application bootstraping from index.php to bootstrap.php

--- demo/index.php
+++ demo/index.php
@@ -15,40 +15,7 @@
  *
  ******************************************************************************/
 
-// Define important paths as global constants
-
-define('__ROOT_PATH', '../');
-define('__APP_PATH', __ROOT_PATH . 'demo/');
-define('__FF_PATH', __ROOT_PATH . 'framework/');
-
-// setup autoloader ( for demo application as well as the framework classes )
-
-require_once __FF_PATH . 'classes/AutoLoader.php';
-
-$loader = new AutoLoader();
-$loader->addPath(__FF_PATH . 'classes/')
-       ->addPath(__FF_PATH . 'events/')
-       ->addPath(__FF_PATH . 'http/')
-       ->addPath(__FF_PATH . 'interface/')
-       ->addPath(__APP_PATH . 'classes/')
-       ->addPath(__APP_PATH . 'commands/')
-       ->addPath(__APP_PATH . 'filters/')
-       ->addPath(__APP_PATH . 'helpers/');
-
-spl_autoload_register(array($loader, 'load'));
-
-// create request, response and registry objects
-// register event handlers
-// add the authentication filter
-// let the FrontController do its work
-
-$dispatcher = EventDispatcher::getInstance();
-$request = new HttpRequest();
-$response = new HttpResponse();
-$registry = new Registry();
-$registry->setRequest($request);
-$registry->setResponse($response);
-$registry->set('EventDispatcher', $dispatcher);
+require_once 'bootstrap.php';
 
 // if running on a local lampp, set IP adress to 127.0.0.1 in order to block
 // yourself
@@ -56,6 +23,7 @@ $ipCheckHandler = new IpCheckHandler($registry, array('127.0.0.2'));
 $authLoggingHandler = new AuthLoggingHandler($registry,
                                              __APP_PATH . 'logs/auth.log');
 
+$dispatcher = $registry->get('EventDispatcher');
 $dispatcher->addHandler('onLogin', $ipCheckHandler);
 $dispatcher->addHandler('onInvalidLogin', $authLoggingHandler);
 
@@ -63,4 +31,4 @@ $resolver = new FileSystemCommandResolver(__APP_PATH . 'commands', 'Default');
 
 $controller = new FrontController($resolver);
 $controller->addPreFilter(new HttpAuthFilter($registry, array('user' => 'password')));
-$controller->handleRequest($request, $response);
+$controller->handleRequest($registry->getRequest(), $registry->getResponse());
---
+++ framework/bootstrap.php
@@ -0,0 +1,54 @@
+<?php
+
+/*******************************************************************************
+ *
+ * Application default bootstrap file
+ *
+ * TODO:
+ * Replace all occurences of {app} by your application's directory name.
+ *
+ * When bootstrapping your application, after this file has been included, the
+ * following objects and variables are available:
+ * - constants:
+ *   > __ROOT_PATH
+ *   > __APP_PATH
+ *   > __FW_PATH
+ * - $registry - the Registry
+ * - $registry->getRequest() - the HttpRequest
+ * - $registry->getResponse() - the HttpResponse
+ * - $registry->get('EventDispatcher') - the EventDispatcher
+ *
+ * @package {app}
+ * @author Benjamin Ulmer
+ * @link http://github.com/remluben/rsf
+ *
+******************************************************************************/
+
+$appName = '{app}';
+
+define('__ROOT_PATH', dirname(dirname(realpath(__FILE__))) . '/');
+define('__APP_PATH', __ROOT_PATH . $appName . '/');
+define('__FW_PATH', __ROOT_PATH . 'framework/');
+
+require_once __FW_PATH . 'classes/AutoLoader.php';
+
+$loader = new AutoLoader(array(
+        __FW_PATH . 'classes/',
+        __FW_PATH . 'events/',
+        __FW_PATH . 'http/',
+        __FW_PATH . 'interface/',
+        __APP_PATH . 'classes/',
+        __APP_PATH . 'commands/',
+        __APP_PATH . 'filters/',
+        __APP_PATH . 'helpers/',
+));
+
+spl_autoload_register(array($loader, 'load'));
+
+$dispatcher = EventDispatcher::getInstance();
+$request = new HttpRequest();
+$response = new HttpResponse();
+$registry = new Registry();
+$registry->setRequest($request);
+$registry->setResponse($response);
+$registry->set('EventDispatcher', $dispatcher);
